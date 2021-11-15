1f75a670e5903d463f987319ae772d95a922bf5b
ahmedp-97 <ahmedpiric997@gmail.com>
2021-11-15 16:36:20 +0100

    css update file

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,7 +3,7 @@ import React from "react";
 function NewsArticle({ data }) {
   return (
     <div className="news">
-      <img width="450" height="345" src={data.urlToImage} />
+      <img width="450" height="305" src={data.urlToImage} />
       <h1 className="news_title">{data.title}</h1>
       <p className="news_desc">{data.description}</p>
       <button>Read more</button>
