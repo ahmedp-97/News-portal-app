83922622fcb8b03cabfcb2231c6c29af5e236b45
ahmedp-97 <ahmedpiric997@gmail.com>
2021-11-15 13:29:18 +0100

    uploading files

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "07ced3f253124404846aecf798b80d30";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
---
+++ src/components/NewsArticle.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NewsArticle({ data }) {
+  return (
+    <div className="news">
+      <img width="450" height="345" src={data.urlToImage}/>
+      <h1 className="news_title">{data.title}</h1>
+      <p className="news_desc">{data.description}</p>
+      <button>Read more</button>
+    </div>
+  );
+}
+
+export default NewsArticle;
+
+
+
