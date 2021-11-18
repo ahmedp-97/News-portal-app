49b54f5a433a0a4c382a1c8c06c697d3e3c6cc87
ahmedp-97 <ahmedpiric997@gmail.com>
2021-11-18 16:09:22 +0100

    new nav and links work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { NewsContextProvider } from "./NewsContext";
+import { Routes, Route, Link } from "react-router-dom";
 import News from "./components/News";
-import Navbar from "./components/Navbar/Navbar";
-import "./components/Navbar/Navbar.css";
 import "./app.css";
 
 function App() {
   return (
-    <NewsContextProvider>
-      <Navbar />
-      <News />
-    </NewsContextProvider>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="article" element={<Article />} />
+            <Route path="search" element={<Search />} />
+           </Routes>
+        
+      
+        
+      
+  );
+}
+
+function Home() {
+  return (
+    <>
+    <nav className="navbarItems">
+    <h1 className="pageName">News Portal App</h1>
+    <li className="navMenu">
+      <Link to="/article">Article</Link>
+      <Link to="/search">Search</Link>
+    </li> 
+      </nav>
+      <main>
+      <NewsContextProvider>
+        <News />
+      </NewsContextProvider>
+      </main>
+      
+    </>
+  );
+}
+
+function Article() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+function Search() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { NewsContext } from "../NewsContext";
 import NewsArticle from "./NewsArticle";
 
-
 function News(props) {
   const { data } = useContext(NewsContext);
   console.log(data);
@@ -14,7 +13,7 @@ function News(props) {
           ? data.articles.map((news) => (
               <NewsArticle data={news} key={news.url} />
             ))
-          : "Loading"}
+          : ""}
       </div>
     </div>
   );
